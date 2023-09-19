1a85388d70b7ff1eb5e39072ad543954f7a15acc
bheemayoganand <bheemayoganand27@gmail.com>
2023-09-19 19:45:26 +0530

    added new logics for swapping program

--- src/test/java/harish_package/swappingNumbers.java
+++ src/test/java/harish_package/swappingNumbers.java
@@ -4,6 +4,15 @@ public class swappingNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=200,b=500;
+		System.out.println("Before swapping values are.."+a+"  "+b);
+		
+		//logic 1 - Third Variable
+		int t=a;
+		    a=b;
+		    b=t;
+		    System.out.println("After swapping values are.."+a+"  "+b);
 
 	}
 
---
+++ src/test/java/harish_package/swapwithoutusingtemp.java
@@ -0,0 +1,22 @@
+package harish_package;
+
+public class swapwithoutusingtemp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=300, b=400;
+		
+		System.out.println("Before swapping values are.."+a+"  "+b);
+		
+		a=a+b;//300+400=700
+		
+		b=a-b;//700-400=300
+		
+		a=a-b;//700-300=400
+		
+		
+		System.out.println("After swapping values are.."+a+"  "+b);
+	}
+
+}

396c2b1077d397ada25b07f3ba28496546463231
gowriswetha <gowriswetha27@gmail.com>
2023-09-20 16:26:10 -0400

    gowri add new program

--- src/test/java/gowri/variables/gvpractice.java
+++ src/test/java/gowri/variables/gvpractice.java
@@ -4,7 +4,28 @@ public class gvpractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      //type variableName= value;
+		//create a variable called name of type string and assign it the value "maakkshardh"
+		
+		String name="maakkshardh";
+		System.out.println(name);
+		
+//create a variable called myNum of type int and assign a value it to 09
+		
+		int myNum= 9;
+		System.out.println(myNum);
+		
+//declare a variable without assign the value in above
+		int Num;
+		Num=20;
+	    System.out.println(Num);
+		
+//change the value number from 9 to 27
+	    int number = 9;
+	    number = 27;
+	    System.out.println(number);
+	    	
 	}
+	
 
 }
---
+++ src/test/java/gowri_practice_package/gowrimethods.java
@@ -0,0 +1,26 @@
+package gowri_practice_package;
+
+public class gowrimethods {
+	
+	public static void main(String[] args) {
+		
+		method();
+		methodone();
+		methodtwo();
+	}
+	public static void method()
+	{
+		System.out.println("Hi..");
+	}
+	
+	public static void methodone()
+	{
+		
+	System.out.println("goood start gowri");
+	}
+	
+	public static void methodtwo()
+	{
+		System.out.println("keep practice");
+	}
+	}

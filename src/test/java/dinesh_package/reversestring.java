ba85e0387c4416ec9d53436c53a2465dc56c8ce4
dineshbethireddy <dinesh.07006@gmail.com>
2023-09-22 13:28:53 +0530

    reversestring class is added by dinesh

---
+++ src/test/java/dinesh_package/reversestring.java
@@ -0,0 +1,22 @@
+package dinesh_package;
+
+public class reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String str="BRSD";
+		String rev="";
+		int len=str.length();
+		
+		for(int i = len-1; i>=0; i--)
+		{
+			rev=rev+str.charAt(i);
+			
+		}
+		System.out.println("reversed string is: " +rev);
+				
+
+	}
+
+}

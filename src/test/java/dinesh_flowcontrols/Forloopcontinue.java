7936743e28db5c06fe33dec74c177c3a3d465f16
dineshbethireddy <dinesh.07006@gmail.com>
2023-09-24 13:39:53 +0530

    dinesh added switch practice class

---
+++ src/test/java/dinesh_flowcontrols/Forloopcontinue.java
@@ -0,0 +1,19 @@
+package dinesh_flowcontrols;
+
+public class Forloopcontinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=100;i++)
+		{
+			if ( i==8 || i==16 || i==35|| i==72 || i==91 || i==97)
+			{
+				continue;
+			}
+		System.out.println(i);
+		}
+		
+	}
+
+}

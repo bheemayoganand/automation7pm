61871c6a284b546477cb8794ea20e3d6a074f3b2
gowriswetha27 <gowriswetha27@gmail.com>
2023-09-24 14:48:09 -0400

    new class added by gowri

---
+++ src/test/java/gowriflowcontrol/gifelse_operaters.java
@@ -0,0 +1,29 @@
+package gowriflowcontrol;
+
+public class gifelse_operaters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age = 90;
+        if (age < 25) {
+	    System.out.println("age is greater than 50 so, old"); 
+	    }
+	    else if (age <= 50 && age >92) {
+	    	System.out.println("middle age"); 
+	    }
+	    else if (age <=33 && age == 25) {
+        	System.out.println("young age");   
+        }
+	    else if (age ==50 || age < 89) {
+	    	System.out.println("old");
+	    }
+	    else {
+	    	System.out.println("young");
+	     	
+	    }
+        
+        
+        }
+        
+
+}

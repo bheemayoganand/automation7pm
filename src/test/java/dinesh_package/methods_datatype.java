dc900b90f257c94f57a5f8840568e561aa66cd65
dineshbethireddy <dinesh.07006@gmail.com>
2023-09-27 23:01:39 +0530

    dinesh added new class

---
+++ src/test/java/dinesh_package/methods_datatype.java
@@ -0,0 +1,33 @@
+package dinesh_package;
+
+public class methods_datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("dineshbrs");
+		methodone();
+		methodtwo();
+		
+		
+	}
+	
+
+	//methods are to be created with in the class only
+
+	// methods need to be called within the methods only. 
+	// we shouldn't create a method inside other method
+	
+	public static void methodone() {
+		// TODO Auto-generated method stub
+		System.out.println("methodone");
+				
+	}
+
+	public static void methodtwo() {
+		System.out.println("methodtwo");
+}
+		
+	}
+	
+

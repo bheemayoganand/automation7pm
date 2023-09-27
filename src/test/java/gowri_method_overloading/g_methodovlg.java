cd3520c9c0ddd7591c3e765801b1c724996963b7
gowriswetha27 <gowriswetha27@gmail.com>
2023-09-27 14:48:53 -0400

    new code added by gowri

--- src/test/java/gowri_method_overloading/g_methodovlg.java
+++ src/test/java/gowri_method_overloading/g_methodovlg.java
@@ -3,22 +3,22 @@ package gowri_method_overloading;
 public class g_methodovlg {
 
  public static void methodgowri(int number,String s1, float f1) {
-	 System.out.println("methodgowri");
+	 System.out.println(number + " ," + s1 + " , "+ f1);
  }
  public static void methodgowri(String s2, float f2, int number) {
-	 System.out.println("methodgowri");
+	 System.out.println(s2+" ,"+f2 +" ," + number);
  }
 public static void methodgowri(int number, float f3, String s3) {
-	System.out.println("methodgowri");
+	System.out.println(number+","+ f3+","+s3);
 }
 public static void methodgowri(String s4, int number) {
-	 System.out.println("methodgowri");
+	 System.out.println(s4+","+number);
 }	
 public static void methodgowri(int number,float f5) {
-	 System.out.println("methodgowri");
+	 System.out.println(number+","+f5);
 }	
 public static void methodgowri(String s6, float f6) {
-	 System.out.println("methodgowri");
+	 System.out.println(s6+","+f6);
 }	
 	
 	public static void main(String[] args) {
